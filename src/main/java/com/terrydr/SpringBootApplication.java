8b86b7d11709ae87d51c89e0791824852e0d5dda
YanZhengYuan <hank.zyyan@outlook.com>
2018-04-14 16:57:14 +0800

    Initial commit

---
+++ src/main/java/com/terrydr/SpringBootApplication.java
@@ -0,0 +1,25 @@
+package com.terrydr;
+
+/**
+ * Copyright (C), 2018-2020, NanJing Terrydr. Co., Ltd.
+ *
+ * @Package: com.terrydr
+ * @Description: Spring Boot 程序入口
+ * @author: YanZhengYaun
+ * @Date: 2018/4/14 16:11
+ * @version: 1.00
+ */
+public class SpringBootApplication {
+
+    /**
+    * @Description: 程序入口方法
+    * @param args
+    * @return
+    * @throws
+    * @author YanZhengYaun
+    * @date 2018/4/14 16:32
+    */
+    public static void main(String[] args) {
+
+    }
+}
